9c4af1d60f8a2f4c3696697960d22ece15114824
comcloud <2230817302@qq.comm>
2020-03-23 13:50:10 +0800

    four commit

--- src/main/resources/db/migration/V2_Create_user_table.sql
+++ src/main/resources/db/migration/V2_Create_user_table.sql
@@ -8,3 +8,20 @@ create table user(
     --用户购买时间
     utime datetime
 )
+
+DELIMITER $$
+CREATE PROCEDURE pre()
+BEGIN
+DECLARE i INT;
+SET i=1;
+WHILE i<1000 DO
+    INSERT INTO goods(pwd,STATUS) VALUES(1234568+i,1);
+SET i=i+1;
+END WHILE;
+END
+$$
+CALL pre();
+
+DELETE FROM goods
+
+SELECT * FROM goods;
